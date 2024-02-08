bf411f5964771429cc3fa5ab05ed3512a284007b
Ivo <ivaylo.golemanski@gmail.com>
2024-02-08 03:07:11 +0200

    uploading additional modules

---
+++ HINTS/modules.py
@@ -0,0 +1,16 @@
+# Module "random" is used when we need randomply generated number. We need to use it with its own functions like "random.random()" or "random.randint()" etc.
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+
+
+# Module "math" is used for different types of calculations. It is used with its own functions like "math.ceil()" etc.
+# import math
+# def paint_calc(height, width, cover):
+#     num = math.ceil((height * width) / cover) 
+#     print(f"You'll need {round(num)} cans of paint.")
+    
+# test_h = int(input("Enter wall height: ")) # Height of wall (m)
+# test_w = int(input("Enter wall width: ")) # Width of wall (m)
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
