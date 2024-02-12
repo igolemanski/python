c88fcb9e1e318136621b1c8c6020647ba7ca0aa5
Ivaylo Golemanski <igolemanski@gmail.com>
2024-02-12 23:07:14 +0200

    combining 2 functions in one. Still have to be fixed

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,25 +4,29 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text. 
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+
+def caesar(direction, plain_text, shift_amount):
+    if direction == "encode":
+        caesar(plain_text=text, shift_amount=shift)
+    elif direction == "decode":
+        caesar(cipher_text=text, shift_amount=shift)
+
     cipher_text = ""
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-        # list.index(x[, start[, end]])Return zero-based index in the list of the first item whose value is equal to x. Raises a ValueError if there is no such item.
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+        cipher_text += alphabet[new_position]
+
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
     
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. \
-encrypt(plain_text=text, shift_amount=shift)
+    print(f"The decoded text is {plain_text}")
+    print(f"The encoded text is {cipher_text}")
+
+
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(direction, plain_text=text, shift_amount=shift)
