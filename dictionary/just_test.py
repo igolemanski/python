213f3b9d05092c54dd74620f287c9127bfe156c9
Ivo <ivaylo.golemanski@gmail.com>
2024-03-02 13:16:37 +0200

    latest commit

---
+++ dictionary/just_test.py
@@ -0,0 +1,9 @@
+# 1. Accessing Values: Create a dictionary representing a person's information (name, age, city), and then print out each piece of information separately.
+person_info = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+print(person_info["age"])
+print(person_info["name"])
+print(person_info["city"])
