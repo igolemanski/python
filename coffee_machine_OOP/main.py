bbf62a562fadea2b75aa56b9a7ae0bc136debc0c
Ivo <ivaylo.golemanski@gmail.com>
2024-08-05 16:28:23 +0300

    completing the latest file update

--- coffee_machine_OOP/main.py
+++ coffee_machine_OOP/main.py
@@ -1,15 +1,12 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
+
 is_on = True
-
-coffee_maker.report()
-money_machine.report()
-
 
 while is_on:
     options = menu.get_items()
@@ -18,8 +15,9 @@ while is_on:
         is_on = False
     elif choice == "report":
         coffee_maker.report()
-        money_machine. report()
+        money_machine.report()
     else:
         drink = menu.find_drink(choice)
+
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
